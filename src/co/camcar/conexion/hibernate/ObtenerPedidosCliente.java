f4cfb6c23002b2d7fe66607a09ae1dbe371f7654
Cristian Cardozo <cristian.cardozo@ticketmaster.com>
2022-12-01 08:55:36 -0500

    Ejemplo como puede manejar consultas con fetch tipo lazy si se cierra la sesión previamente.

--- src/co/camcar/conexion/hibernate/ObtenerPedidosCliente.java
+++ src/co/camcar/conexion/hibernate/ObtenerPedidosCliente.java
@@ -3,6 +3,7 @@ package co.camcar.conexion.hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 public class ObtenerPedidosCliente {
 
@@ -16,10 +17,16 @@ public class ObtenerPedidosCliente {
 		try {
 			session.beginTransaction();
 			// Obtener cliente
-			Cliente cliente = session.get(Cliente.class, 7);
+			//Cliente cliente = session.get(Cliente.class, 7);
+			Query<Cliente> consulta = session.createQuery("SELECT cl FROM Cliente cl JOIN FETCH cl.pedidos WHERE cl.id=:clienteId", Cliente.class);
+			consulta.setParameter("clienteId", 7);
+			Cliente cliente = consulta.getSingleResult();
+			
 			System.out.println("Cliente: "+cliente);
-			System.out.println("Pedidos: "+cliente.getPedidos());
+
 			session.getTransaction().commit();
+			session.close();
+			System.out.println("Pedidos: "+cliente.getPedidos());
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
